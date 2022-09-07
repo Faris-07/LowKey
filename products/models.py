04cf02bef1b96e612531eeb11ae503220c095b4a
Faris-07 <farisuddin03@gmail.com>
2022-09-07 20:08:27 +0000

    Add Product model to products model.py

--- products/models.py
+++ products/models.py
@@ -19,3 +19,25 @@ class Category(models.Model):
         """ return name to display on site """
         return self.name_to_display
 
+
+product_size_choices = (
+    ("S", "S"),
+    ("M", "M"),
+    ("L", "L"),
+    ("XL", "XL"),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    size = models.CharField(max_length=2, choices=product_size_choices, default="M")
+    composition = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
