1c7df6c72b2dcb3766040e78181709897b4caa45
Faris-07 <farisuddin03@gmail.com>
2022-09-11 18:01:50 +0000

    Add product view and path

--- products/admin.py
+++ products/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
 class CategoryAdmin(admin.ModelAdmin):
     """ Category admin management """
     list_display = ("category_name", "name_to_display")
 
+
 class ProductAdmin(admin.ModelAdmin):
     """ Product admin management """
     list_display = ("sku", "pk", "name", "category", "size", "price", "image", "category_id")
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
