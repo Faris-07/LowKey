fb0ea60a8b8ec13cdd954562a9efaeb7f2abeee7
Faris-07 <farisuddin03@gmail.com>
2022-09-14 17:10:22 +0000

    Add base product detail view

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
-def all_products(request):
+def AllProductsView(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
@@ -13,3 +13,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def ProductDetail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
