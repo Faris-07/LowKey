97863667eca7db3f8431ce3e066c9867c8524ee6
Faris-07 <farisuddin03@gmail.com>
2022-09-16 18:16:23 +0000

    Add bag view

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def BagView(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
