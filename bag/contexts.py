8d5d5020d7e41179babc806cf383b716a6a490dc
Faris-07 <farisuddin03@gmail.com>
2022-09-19 16:53:42 +0000

    Add quantity buttons to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -24,14 +24,14 @@ def bag_contents(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
+            for item_size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
-                    'size': size,
+                    'size': item_size,
                 })
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
