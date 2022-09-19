1fef45f560905f31392ee533a627e32299b6a94d
Faris-07 <farisuddin03@gmail.com>
2022-09-19 17:16:44 +0000

    Add adjust bag and remove from bag functions to views and update bag view and add to bag view to comply with PEP8 standards

--- bag/views.py
+++ bag/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 
 # Create your views here.
 
-def BagView(request):
+def bag_view(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
-def AddToBag(request, item_id):
+def add_to_bag(request, item_id):
     """ Add a chosen quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
@@ -33,3 +33,33 @@ def AddToBag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """ Adjust item's quantity in bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity') or 1)
+    bag = request.session.get('bag', {})
+
+    if quantity > 99:
+        messages.error(
+            request, 'Sorry, value must be less then or equal to 99.')
+    elif quantity > 0:
+        bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Removes item from bag """
+    product = get_object_or_404(Product, pk=item_id)
+    bag = request.session.get('bag', {})
+    bag.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your bag')
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
