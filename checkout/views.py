b6552b2b866ef9e81b73b115ecf1b949d0578f3c
Faris-07 <farisuddin03@gmail.com>
2022-09-23 16:44:06 +0000

    Add stipe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LQupnH0kWMFctTJKlOmJTaUBQUzJel34fgGvRb8bVkVRdzp7QTv3yg4F7DNVywxQ5iyOqactAosGjxdkgaHI3fM00imD472qA',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
