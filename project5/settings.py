6ad11c66fdd87c24feb60b680889be219c5f7d15
Faris-07 <farisuddin03@gmail.com>
2022-09-23 18:11:47 +0000

    Add basic checkout functionality

--- project5/settings.py
+++ project5/settings.py
@@ -168,6 +168,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
