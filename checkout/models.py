d6b856a08e9b9ed7999599756e9301253f38fdc7
Faris-07 <farisuddin03@gmail.com>
2022-09-24 14:04:13 +0000

    Add country placeholder label in order model

--- checkout/models.py
+++ checkout/models.py
@@ -19,7 +19,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     order_date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(
