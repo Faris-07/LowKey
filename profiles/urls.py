1938c87a26e760290c9df83e791e828101f871ea
Faris-07 <farisuddin03@gmail.com>
2022-09-24 17:43:26 +0000

    Add user orders url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile')
+    path('', views.profile, name='profile'),
+    path('user_orders/<order_number>', views.user_orders, name='user_orders'),
 ]
