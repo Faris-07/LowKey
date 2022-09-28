82a0d69233ba22319bd45932d4c3a35155d84394
Faris-07 <farisuddin03@gmail.com>
2022-09-28 19:04:37 +0000

    Add formatting to profile models.py

--- profiles/models.py
+++ profiles/models.py
@@ -14,13 +14,17 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True,
+                                            blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True,
+                                               blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True,
+                                               blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True,
+                                   blank=True)
     registration_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
