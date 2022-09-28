cc4c6a86255e73a6a4ca0d92ca73cd6ead4cd355
Faris-07 <farisuddin03@gmail.com>
2022-09-28 17:33:54 +0000

    Add notations and formatting to products forms.py

--- products/forms.py
+++ products/forms.py
@@ -4,18 +4,22 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    """ Product Form """
 
     class Meta:
+        """ Gets all fields from the Product model """
         model = Product
-        fields = '__all__'
+        fields = "__all__"
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         names_to_display = [(c.id, c.get_name_to_display()) for c in categories]
 
-        self.fields['category'].choices = names_to_display
+        self.fields["category"].choices = names_to_display
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black text-dark rounded-0'
+            field.widget.attrs["class"] = "border-black text-dark rounded-0"
