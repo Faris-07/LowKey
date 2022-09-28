b497e29c17aa83d62eeb4d0043376b1c03eb4e8a
Faris-07 <farisuddin03@gmail.com>
2022-09-28 19:07:49 +0000

    Add notations and formatting to profile views.py

--- profiles/views.py
+++ profiles/views.py
@@ -3,18 +3,17 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
-from .forms import UserProfileForm
-
 from checkout.models import Order
 from products.models import Product
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 @login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-    products = Product.objects.filter(likes=request.user.id) 
+    products = Product.objects.filter(likes=request.user.id)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -22,7 +21,8 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Update failed.\
+                 Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all().order_by('-order_date')
@@ -37,13 +37,15 @@ def profile(request):
 
     return render(request, template, context)
 
+
 def user_orders(request, order_number):
+    """ Displays the user's previous orders """
     order = get_object_or_404(Order, order_number=order_number)
     order_date = order.date.strftime("%d-%m-%Y %H:%M:%S")
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
+        f'A confirmation email was sent on the {order_date}.'
     ))
 
     template = 'checkout/checkout_success.html'
