1ec131370c465e2b61847d40d2f62affe0169ec6
Faris-07 <farisuddin03@gmail.com>
2022-09-28 17:28:52 +0000

    Add formatting to products admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,28 @@
-from django.contrib import admin
 from .models import Product, Category
+from django.contrib import admin
 
 # Register your models here.
 
+
 class CategoryAdmin(admin.ModelAdmin):
     """ Category admin management """
+
     list_display = ("category_name", "name_to_display")
 
 
 class ProductAdmin(admin.ModelAdmin):
     """ Product admin management """
-    list_display = ("sku", "pk", "name", "category", "size", "price", "image", "category_id")
+
+    list_display = (
+        "sku",
+        "pk",
+        "name",
+        "category",
+        "size",
+        "price",
+        "image",
+        "category_id",
+    )
 
 
 admin.site.register(Product, ProductAdmin)
