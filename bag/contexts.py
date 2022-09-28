c4705c1668b111e726d8f0a9724cc4ba0a4694b2
Faris-07 <farisuddin03@gmail.com>
2022-09-28 17:58:11 +0000

    Add notations to bag contexts.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,11 @@
-from django.shortcuts import get_object_or_404
-from products.models import Product
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
-
+    """ Handles Bag Contents """
     bag_items = []
     total = 0
     product_count = 0
@@ -37,7 +38,7 @@ def bag_contents(request):
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
 
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
