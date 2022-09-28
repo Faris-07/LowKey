0ae6264f350c5f68d116b62c135f829980184f34
Faris-07 <farisuddin03@gmail.com>
2022-09-28 20:44:26 +0000

    Update secret key for heroku deployment

--- project5/settings.py
+++ project5/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-2$3*19-zc@s+9&38rxw=fy!fy84y54$9$&efy02#%dv2lt0r0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
